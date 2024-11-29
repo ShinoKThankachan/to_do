afb94f25f38e1e08a0730ce3695a73fce9920bfb
ShinoKThankachan <shinothankachan17@gmail.com>
2024-11-29 11:31:24 +0530

    files added

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+subject=[]
+
+
 def index(request):
-    return render(request,'index.html')
+    if request.method=='POST': 
+        l=len(subject)
+        slno=l+1
+        sub=request.POST['text']
+        subject.append({'slno':slno,'sub':sub})
+    return render(request,'index.html' ,{'subjects':subject})
+def todo_update(request,slno):
+    for i in subject:
+        if i['slno']==slno:
+            sub=i['sub']
+            if request.method=='POST':
+                todo_sub=request.POST['todo']
+                for i in subject:
+                    if i['slno']==slno:
+                        i['sub']=todo_sub
+                    return redirect(index)
+        return render(request,'todo_update.html',{'sub':sub,'slno':slno})
